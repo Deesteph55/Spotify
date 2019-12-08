f88844dc133dd641682573837e9504c324c7e565
Deesteph55 <deedee704@ymail.com>
2019-12-08 14:42:20 -0600

    api tested commit

--- spotify-app/src/App.js
+++ spotify-app/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./spotify-logo.jpg";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+  state = {
+    contacts: []
+  };
+
+  componentDidMount() {
+    axios.get("http://jsonplaceholder.typicode.com/users").then(response => {
+      console.log(response);
+      this.setState({
+        contacts: response.data
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <h1>Spotify</h1>
+        <ul>
+          {this.state.contacts.map(contact => (
+            <li key={contact.id}>{contact.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;

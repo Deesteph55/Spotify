c490abdbecd13e11c0047ec96e6f802103e066d7
Deesteph55 <deedee704@ymail.com>
2020-02-06 17:31:54 -0600

    unignored build in spotify app

---
+++ spotify-app/build/precache-manifest.6860114897e2d9cf642733ed13f9cb7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c4830e050804f2da2808922de28be9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7585d3b71d1b56c678ed",
+    "url": "/static/css/main.da15b6f3.chunk.css"
+  },
+  {
+    "revision": "8bdfdbcbf56811cda8a7",
+    "url": "/static/js/2.4d3140fb.chunk.js"
+  },
+  {
+    "revision": "3d9880ee0da2a100dc4a6223e0aed39a",
+    "url": "/static/js/2.4d3140fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7585d3b71d1b56c678ed",
+    "url": "/static/js/main.9508c3f2.chunk.js"
+  },
+  {
+    "revision": "761810b0cf8bfc84ffee",
+    "url": "/static/js/runtime-main.aa425418.js"
+  }
+]);

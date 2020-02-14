ba34795c01f267577471e252dcaa0797e9069e0e
Deesteph55 <deedee704@ymail.com>
2020-02-14 01:55:03 -0600

    moved package and build into outer folder

---
+++ build/precache-manifest.83e62954da5315a6655fc4248f30691a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35f5ca8eb09f5ccd98fadee5bd565c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9ddadc5ee61cfcb72b1",
+    "url": "/static/css/main.da15b6f3.chunk.css"
+  },
+  {
+    "revision": "d4dc0c842b69b1edd8d2",
+    "url": "/static/js/2.28a3fb5d.chunk.js"
+  },
+  {
+    "revision": "3d9880ee0da2a100dc4a6223e0aed39a",
+    "url": "/static/js/2.28a3fb5d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b9ddadc5ee61cfcb72b1",
+    "url": "/static/js/main.e5925a8a.chunk.js"
+  },
+  {
+    "revision": "761810b0cf8bfc84ffee",
+    "url": "/static/js/runtime-main.aa425418.js"
+  }
+]);
